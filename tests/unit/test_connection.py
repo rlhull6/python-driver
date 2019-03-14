01016407214779c576ce0aaff2eda4501c42c648
Alan Boudreault <alan@kovaro.ca>
2019-03-14 16:07:35 -0400

    Add a simple unit test

--- tests/unit/test_connection.py
+++ tests/unit/test_connection.py
@@ -74,6 +74,21 @@ class ConnectionTest(unittest.TestCase):
     def make_msg(self, header, body=""):
         return header + uint32_pack(len(body)) + body
 
+    def test_connection_endpoint(self):
+        endpoint = DefaultEndPoint('1.2.3.4')
+        c = Connection(endpoint)
+        self.assertEqual(c.endpoint, endpoint)
+        self.assertEqual(c.endpoint.address, endpoint.address)
+
+        c = Connection(host=endpoint)  # kwarg
+        self.assertEqual(c.endpoint, endpoint)
+        self.assertEqual(c.endpoint.address, endpoint.address)
+
+        c = Connection('10.0.0.1')
+        endpoint = DefaultEndPoint('10.0.0.1')
+        self.assertEqual(c.endpoint, endpoint)
+        self.assertEqual(c.endpoint.address, endpoint.address)
+
     def test_bad_protocol_version(self, *args):
         c = self.make_connection()
         c._requests = Mock()
@@ -426,3 +441,49 @@ class TimerTest(unittest.TestCase):
         tm.add_timer(t2)
         # Prior to #466: "TypeError: unorderable types: Timer() < Timer()"
         tm.service_timeouts()
+
+
+class DefaultEndPointTest(unittest.TestCase):
+
+    def test_default_endpoint_properties(self):
+        endpoint = DefaultEndPoint('10.0.0.1')
+        self.assertEqual(endpoint.address, '10.0.0.1')
+        self.assertEqual(endpoint.port, 9042)
+        self.assertEqual(str(endpoint), '10.0.0.1:9042')
+
+        endpoint = DefaultEndPoint('10.0.0.1', 8888)
+        self.assertEqual(endpoint.address, '10.0.0.1')
+        self.assertEqual(endpoint.port, 8888)
+        self.assertEqual(str(endpoint), '10.0.0.1:8888')
+
+    def test_endpoint_equality(self):
+        self.assertEqual(
+            DefaultEndPoint('10.0.0.1'),
+            DefaultEndPoint('10.0.0.1')
+        )
+
+        self.assertEqual(
+            DefaultEndPoint('10.0.0.1'),
+            DefaultEndPoint('10.0.0.1', 9042)
+        )
+
+        self.assertNotEqual(
+            DefaultEndPoint('10.0.0.1'),
+            DefaultEndPoint('10.0.0.2')
+        )
+
+        self.assertNotEqual(
+            DefaultEndPoint('10.0.0.1'),
+            DefaultEndPoint('10.0.0.1', 0000)
+        )
+
+    def test_endpoint_resolve(self):
+        self.assertEqual(
+            DefaultEndPoint('10.0.0.1').resolve(),
+            ('10.0.0.1', 9042)
+        )
+
+        self.assertEqual(
+            DefaultEndPoint('10.0.0.1', 3232).resolve(),
+            ('10.0.0.1', 3232)
+        )
